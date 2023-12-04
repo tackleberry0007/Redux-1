70ee792057bc0812c39d3b6dc835e4d99635d30f
tackleberry0007 <pankajprasad4545@gmail.com>
2023-12-04 19:06:17 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-
-import './App.css';
+import products from "./products.json";
+import "./App.css";
+import Product from "./Components/Product";
+import Cart from "./Components/Cart";
 
 function App() {
   return (
-    <>
-    <div>Hello</div>
-
-    </>
+    <div className="App">
+      <Cart />
+      <div className="products">
+        {products.map((product) => (
+          <Product {...product} />
+        ))}
+      </div>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import 'mdb-react-ui-kit/dist/css/mdb.min.css';
-import "@fortawesome/fontawesome-free/css/all.min.css";
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
